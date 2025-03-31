12dd545bde4022b71410ebf9e9f9ba5ed9dfb757
xu1feng <2465549609@qq.com>
2025-03-31 20:28:41 +0800

    feat: 康庄大道，上手 AI MCP 工作流

--- ai-mcp-knowledge-app/src/main/java/edu/xyf/knowledge/Application.java
+++ ai-mcp-knowledge-app/src/main/java/edu/xyf/knowledge/Application.java
@@ -5,11 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Configurable
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 
 }
---
+++ ai-mcp-knowledge-app/src/test/java/edu/xyf/knowledge/test/MCPTest.java
@@ -0,0 +1,67 @@
+package edu.xyf.knowledge.test;
+
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MCPTest {
+
+    @Resource
+    private ChatClient.Builder chatClientBuilder;
+
+    @Autowired
+    private ToolCallbackProvider tools;
+
+    @Test
+    public void test_tool() {
+        String userInput = "有哪些工具可以使用";
+        var chatClient = chatClientBuilder
+                .defaultTools(tools)
+                .defaultOptions(OpenAiChatOptions.builder()
+                        .model("gpt-4o")
+                        .build())
+                .build();
+
+        System.out.println("\n>>> QUESTION: " + userInput);
+        System.out.println("\n>>> ASSISTANT: " + chatClient.prompt(userInput).call().content());
+    }
+
+    @Test
+    public void test() {
+        String userInput = "获取电脑配置";
+//        userInput = "在 /Users/fuzhengwei/Desktop 文件夹下，创建 电脑.txt";
+        userInput = "获取电脑配置 在 C:\\Users\\asus\\Desktop 文件夹下，创建 电脑.txt 把电脑配置写入 电脑.txt";
+
+        var chatClient = chatClientBuilder
+                .defaultTools(tools)
+                .defaultOptions(OpenAiChatOptions.builder()
+                        .model("gpt-4o")
+                        .build())
+                .build();
+
+        System.out.println("\n>>> QUESTION: " + userInput);
+        System.out.println("\n>>> ASSISTANT: " + chatClient.prompt(userInput).call().content());
+    }
+
+    @Test
+    public void cnm() throws IOException {
+        String npxPath = "C:\\Program Files\\nodejs\\npx.cmd"; // Windows
+// String npxPath = "/usr/local/bin/npx"; // macOS/Linux
+        ProcessBuilder processBuilder = new ProcessBuilder(npxPath, "-y", "@modelcontextprotocol/server-filesystem", "C:\\Users\\asus\\Desktop", "C:\\Users\\asus\\Desktop");
+        Process process = processBuilder.start();
+
+    }
+
+}
