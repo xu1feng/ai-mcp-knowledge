ba54177babd2aa145397503ab02ba6dba6752729
2465549609@qq.com <xuyifeng@xuyifengdeMacBook-Air.local>
2025-09-25 17:18:43 +0800

    feat: init project

---
+++ ai-mcp-knowledge-app/src/main/java/cn/bugstack/knowledge/Application.java
@@ -0,0 +1,18 @@
+package cn.bugstack.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Xuyifeng
+ * @Description:
+ * @Date: 2025/9/25 16:49
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
